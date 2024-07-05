8805b52ad989af7c5f2e39b2e9ffdd2725a2fb50
Zagor15 <64365681+Zagor15@users.noreply.github.com>
2024-07-05 18:30:53 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,3 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zagor 15 website</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
     <header>
         <h1>Welcome to Zagor15 website</h1>
     </header>
