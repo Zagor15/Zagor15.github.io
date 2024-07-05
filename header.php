4a7da5073eec6857e68a6f7947644d161998643a
Zagor15 <64365681+Zagor15@users.noreply.github.com>
2024-07-05 18:18:36 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zagor 15 website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Zagor15 website</h1>
+    </header>
+    <nav>
+        <a href="index.html">Home</a>
+        <a href="project.html">Project</a>
+        <a href="#">Contact</a>
+    </nav>
+</head>
