542eaf38e00496168a949a6b7da79b3141309cfa
Zagor15 <64365681+Zagor15@users.noreply.github.com>
2024-07-05 18:45:16 -0400

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-    <header>
-        <h1>Welcome to Zagor15 website</h1>
-    </header>
-    <nav>
-        <a href="index.html">Home</a>
-        <a href="project.html">Project</a>
-        <a href="#">Contact</a>
-    </nav>
