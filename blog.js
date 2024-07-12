50a048d915bb8ce7e8b19453377c667dfe238ab3
Zagor15 <64365681+Zagor15@users.noreply.github.com>
2024-07-12 15:52:32 -0400

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const posts = [
+        {
+            title: "My First Blog Post",
+            date: "July 10, 2024",
+            content: "This is the content of my first blog post. I am excited to start this journey!",
+            image: "images/blog1.jpg"
+        },
+        {
+            title: "Another Interesting Post",
+            date: "July 11, 2024",
+            content: "Here is some more interesting content for my second post. Stay tuned for more updates!",
+            image: "images/blog1.jpg"
+        }
+    ];
+
+    const postsContainer = document.getElementById('posts-container');
+
+    posts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
+
+        postElement.innerHTML = `
+            <h3>${post.title}</h3>
+            <p><em>${post.date}</em></p>
+            <img src="${post.image}" alt="${post.title}">
+            <p>${post.content}</p>
+        `;
+
+        postsContainer.appendChild(postElement);
+    });
+});
